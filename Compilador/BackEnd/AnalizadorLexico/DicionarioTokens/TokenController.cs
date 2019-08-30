c1fda3f64fe72274ab637f746ceab3ec40b145ae
Dimitry Ladislau <dimitryld@hotmail.com>
2019-08-30 15:02:51 -0300

    fuck the police

--- Compilador/BackEnd/AnalizadorLexico/DicionarioTokens/TokenController.cs
+++ Compilador/BackEnd/AnalizadorLexico/DicionarioTokens/TokenController.cs
@@ -14,64 +14,65 @@ namespace Compilador.BackEnd.AnalizadorLexico.DicionarioTokens
 
 		#region --- MONTAGEM DICIONARIO DE DADOS ---
 
-		/* CODIGO PROIBIDO NAO MECHA */
-
-		private OleDbConnection _olecon;
-        private OleDbCommand _oleCmd;
-        private static String _Arquivo = @"C:\Users\Eduardo\Desktop\Projetos\Compilador\Compilador\BackEnd\AnalizadorLexico\DicionarioTokens\TabelaTokens.xlsx";
-        private String _StringConexao = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;ReadOnly=False';", _Arquivo);
-        private String _Consulta;
-
         public static List<Token> ListaTokens = new List<Token>();
 
         public List<Token> IniciarDicionarioTokens()
         {
-            // Conectar com excel.
-            ConectarExcel();
-            // Busca excel com dados da tabela.
-
-            try
-            {
-                _oleCmd.CommandText = "SELECT COD, SIMBOLO FROM [fonte$] ";
-                OleDbDataReader reader = _oleCmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    // Monta os objetos do dicionario tokens.
-                    ListaTokens.Add(new Token(Convert.ToInt32(reader.GetValue(0)), reader.GetValue(1).ToString()));
-				}
-
-                reader.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Erro Fatal!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            ListaTokens.Add(new Token(1, "Program"));
+            ListaTokens.Add(new Token(2, "Label"));
+            ListaTokens.Add(new Token(3, "Const"));
+            ListaTokens.Add(new Token(4, "Var"));
+            ListaTokens.Add(new Token(5, "Procedure"));
+            ListaTokens.Add(new Token(6, "Begin"));
+            ListaTokens.Add(new Token(7, "End"));
+            ListaTokens.Add(new Token(8, "Integer"));
+            ListaTokens.Add(new Token(9, "Array"));
+            ListaTokens.Add(new Token(10, "Of"));
+            ListaTokens.Add(new Token(11, "Call"));
+            ListaTokens.Add(new Token(12, "Goto"));
+            ListaTokens.Add(new Token(13, "If"));
+            ListaTokens.Add(new Token(14, "Then"));
+            ListaTokens.Add(new Token(15, "Else"));
+            ListaTokens.Add(new Token(16, "While"));
+            ListaTokens.Add(new Token(17, "Do"));
+            ListaTokens.Add(new Token(18, "Repeat"));
+            ListaTokens.Add(new Token(19, "Until"));
+            ListaTokens.Add(new Token(20, "Readln"));
+            ListaTokens.Add(new Token(21, "Writeln"));
+            ListaTokens.Add(new Token(22, "Or"));
+            ListaTokens.Add(new Token(23, "And"));
+            ListaTokens.Add(new Token(24, "Not"));
+            ListaTokens.Add(new Token(25, "Identificador"));
+            ListaTokens.Add(new Token(26, "Inteiro"));
+            ListaTokens.Add(new Token(27, "For"));
+            ListaTokens.Add(new Token(28, "To"));
+            ListaTokens.Add(new Token(29, "Case"));
+            ListaTokens.Add(new Token(30, "+ "));
+            ListaTokens.Add(new Token(31, "- "));
+            ListaTokens.Add(new Token(32, "* "));
+            ListaTokens.Add(new Token(33, "/ "));
+            ListaTokens.Add(new Token(34, "["));
+            ListaTokens.Add(new Token(35, "]"));
+            ListaTokens.Add(new Token(36, "("));
+            ListaTokens.Add(new Token(37, ")"));
+            ListaTokens.Add(new Token(38, ":= "));
+            ListaTokens.Add(new Token(39, ":"));
+            ListaTokens.Add(new Token(40, "= "));
+            ListaTokens.Add(new Token(41, "> "));
+            ListaTokens.Add(new Token(42, ">= "));
+            ListaTokens.Add(new Token(43, "< "));
+            ListaTokens.Add(new Token(44, "<= "));
+            ListaTokens.Add(new Token(45, "<> "));
+            ListaTokens.Add(new Token(46, ","));
+            ListaTokens.Add(new Token(47, "; "));
+            ListaTokens.Add(new Token(48, "literal"));
+            ListaTokens.Add(new Token(49, "."));
+            ListaTokens.Add(new Token(50, ".."));
+            ListaTokens.Add(new Token(51, "$"));
 
             // Retorna Lista.
             return ListaTokens;
         }
-
-        #region --- CONTROLE CONEXAO ---
-
-        private void ConectarExcel()
-        {
-            try
-            {
-                _olecon = new OleDbConnection(_StringConexao);
-                _olecon.Open();
-
-                _oleCmd = new OleDbCommand();
-                _oleCmd.Connection = _olecon;
-                _oleCmd.CommandType = CommandType.Text;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Erro Fatal!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-		#endregion
 
 		#endregion
 
